0ec965a6c4486dd05e7eab8fb0373beaa4fe4b52
Bartłomiej Bąk <bpmn2407@gmail.com>
2020-02-18 22:10:35 +0100

    News clickable

--- app/src/main/java/com/example/estudentkrosno/ui/materialy/MaterialyFragment.java
+++ app/src/main/java/com/example/estudentkrosno/ui/materialy/MaterialyFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -14,14 +16,24 @@ import androidx.lifecycle.ViewModelProviders;
 
 import com.example.estudentkrosno.R;
 
+import java.util.List;
+
 public class MaterialyFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
         View root = inflater.inflate(R.layout.fragment_tools, container, false);
-        final TextView textView = root.findViewById(R.id.text_tools);
-        textView.setText("Materialy");
+
+        String[] data = {"One","Two"};
+
+        ListView listView = root.findViewById(R.id.list_items);
+
+        ArrayAdapter<String> listViewAdapter = new ArrayAdapter<String>(
+                getActivity(), android.R.layout.simple_list_item_1, data);
+
+        listView.setAdapter(listViewAdapter);
+
         return root;
     }
 }
