cdd72c673d1ee8a19ed463b1609b729c328c6230
Bartłomiej Bąk <bpmn2407@gmail.com>
2020-02-28 20:07:03 +0100

    Data retrieved!!

--- app/src/main/java/com/example/estudentkrosno/ui/oceny/OcenyFragment.java
+++ app/src/main/java/com/example/estudentkrosno/ui/oceny/OcenyFragment.java
@@ -5,64 +5,66 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.TextView;
+import android.widget.Button;
+import android.widget.EditText;
 
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.estudentkrosno.R;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
-import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
-import com.squareup.picasso.Picasso;
 
-import org.w3c.dom.Text;
-
-import java.util.HashMap;
-import java.util.Map;
-
-import static android.content.ContentValues.TAG;
+import java.util.Objects;
 
 public class OcenyFragment extends Fragment {
 
-    private DatabaseReference databaseReference;
-    private FirebaseAuth mAuth;
-    private View view;
-    private TextView userTextView;
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private OcenaAdapter adapter;
+    RecyclerView recyclerView;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-
-
-        mAuth = FirebaseAuth.getInstance();
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        currentUser.getDisplayName();
-
-        View root = inflater.inflate(R.layout.fragment_gallery, container, false);
-        userTextView = root.findViewById(R.id.userTextView);
-        userTextView.setText(currentUser.getDisplayName()+currentUser.getEmail());
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_oceny, container, false);
+        recyclerView = root.findViewById(R.id.oceny_recycler);
         return root;
     }
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        setUpRecyclerView();
+    }
+
+    private void setUpRecyclerView(){
+        Query query = db.collection("Oceny");
+        FirestoreRecyclerOptions<OcenaModel> options = new FirestoreRecyclerOptions.Builder<OcenaModel>()
+                .setQuery(query, OcenaModel.class)
+                .build();
+
+        adapter = new OcenaAdapter(options);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.setAdapter(adapter);
+    }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+        adapter.startListening();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        adapter.stopListening();
+    }
 }
